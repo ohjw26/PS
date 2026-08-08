e74abcb9a08ffda7979c347646f5fa6b1177a468
JIWON OH <ohjiwon3040@gmail.com>
2026-08-09 01:58:38 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.02 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -1,12 +1,6 @@
 class Solution {
     public int solution(int slice, int n) {
-        int answer = 0;
-        
-        if (n % slice == 0) {
-            answer = n / slice;
-        } else if (n % slice != 0) {
-            answer = n / slice + 1;
-        }
+        int answer = (n % slice == 0) ? n / slice : n / slice + 1;
         return answer;
     }
 }

9951302dcd414d15b170fb0711d99cc360cf45c9
JIWON OH <ohjiwon3040@gmail.com>
2026-08-09 02:27:48 +0900

    [level 0] Title: 배열의 평균값, Time: 4.27 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
@@ -1,13 +1,7 @@
+import java.util.Arrays;
+
 class Solution {
     public double solution(int[] numbers) {
-        double answer = 0;
-        
-        for (int i = 0; i < numbers.length; i++) {
-            answer += numbers[i];
-        }
-        
-        answer /= numbers.length;
-        
-        return answer;
+        return Arrays.stream(numbers).average().orElse(0);
     }
 }
